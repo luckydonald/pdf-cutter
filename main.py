ca24f18e22792f8d48f1a8205f51e501f649f623
luckydonald <mac2019.git@luckydonald.de>
2021-07-16 14:54:26 +0200

    initial working

---
+++ main.py
@@ -0,0 +1,180 @@
+import base64
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from PIL import Image
+from io import BytesIO
+from fitz import fitz  # PyMuPDF
+
+PAGE = 1
+
+PDF_BASE64file = BytesIO()
+with open('sample.pdf', 'br') as f:
+    base64.encode(f, PDF_BASE64file)
+# end if
+# PDF_BASE64 = b''
+PDF_BASE64 = PDF_BASE64file.getvalue()
+
+pdf_file_bin = BytesIO(base64.decodebytes(PDF_BASE64))
+pdf_file_bin.seek(0)
+
+
+# https://stackoverflow.com/q/42733539/3423324#convert-pdf-page-to-image-with-pypdf2-and-bytesio
+# Open PDF Source
+
+
+
+
+with open('cropper.html') as f:
+    html = f.read()
+# end if
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        self.do_stuff()
+    # end def
+
+    def do_GET(self):
+        self.do_stuff()
+    # end def
+
+    def do_stuff(self):
+        # Send a notification when a request is made
+        # notification = nc.Notification()
+        # notification.message = "Made GET Request"
+        # nc.schedule_notification(notification, 1, False)
+        print('MADE GET')
+
+        # Send "Hello World"
+        if self.path == '/crop':
+            self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+            data = {
+                "coordinates": {
+                    "width": 490,
+                    "height": 693,
+                    "left": 61,
+                    "top": 87
+                },
+                "image": {
+                    "width": 612,
+                    "height": 866
+                }
+            }
+            data = json.loads(self.data_string)
+
+            img_w, img_h = data['image']['width'],  data['image']['height']
+            select_w, select_h = data['coordinates']['width'],  data['coordinates']['height']
+            select_l, select_t = data['coordinates']['left'],  data['coordinates']['top']
+
+            percent_w = select_w / img_w
+            percent_h = select_h / img_h
+            percent_l = select_l / img_w
+            percent_t = select_t / img_h
+
+            pdf_file_bin.seek(0)
+            pdf_file = PdfFileReader(pdf_file_bin)
+            page = pdf_file.getPage(PAGE)
+            print(page.cropBox.getUpperLeft())
+            print(page.cropBox.getUpperRight())
+            print(page.cropBox.getLowerLeft())
+            print(page.cropBox.getLowerRight())
+            print(data)
+
+            pdf_w, pdf_h = page.cropBox.getWidth(), page.cropBox.getHeight()
+            pdf_l, pdf_t = page.cropBox.getUpperLeft()
+            pdf_l, pdf_t, pdf_w, pdf_h = float(pdf_l), float(pdf_t), float(pdf_w), float(pdf_h)
+
+            print(pdf_l, pdf_t, pdf_w, pdf_h)
+            pdf_t = pdf_h - pdf_t  # needs to be inverted
+
+            print(pdf_l, pdf_t, pdf_w, pdf_h)
+
+            new_w = pdf_w * percent_w
+            new_h = pdf_h * percent_h
+            new_l = pdf_l + (pdf_w * percent_l)
+            new_t = pdf_t + (pdf_h * percent_t)
+            print(new_l, new_t, new_w, new_h)
+            new_t = pdf_h - new_t  # needs to be inverted
+            print(new_l, new_t, new_w, new_h)
+
+            page.cropBox.upperLeft = (new_l, new_t)
+            page.cropBox.lowerRight = (new_l + new_w, new_t - new_h)
+            print(((new_l, new_t), (new_l + new_w, new_t - new_h)))
+
+            print(page.cropBox.getUpperLeft())
+            print(page.cropBox.getUpperRight())
+            print(page.cropBox.getLowerLeft())
+            print(page.cropBox.getLowerRight())
+
+            output_file = BytesIO()
+
+            output_pdf = PdfFileWriter()
+            output_pdf.addPage(page)
+            output_pdf.write(output_file)
+
+            content_type = 'application/json'
+            base64_data = base64.encodebytes(output_file.getvalue())
+            base64_data = base64_data.replace(b'\n', b'')
+            message = b'{"filename": "a.pdf", "base64": "' + base64_data + b'"}'
+
+            # content_type = 'application/pdf'
+            # message = output_file.getbuffer()
+        elif self.path == '/preview':
+            # https://stackoverflow.com/a/54001356/3423324
+            file = self.rfile.read(int(self.headers['Content-Length']))
+            doc = fitz.open(stream=file, filetype='application/pdf')
+            page = doc.loadPage(PAGE)
+            pix = page.getPixmap()
+            print(pix, len(pix.samples))
+            mode = None
+            if len(pix.samples) == 3 * pix.width * pix.height:
+                mode = "RGB"
+            elif len(pix.samples) == 4 * pix.width * pix.height:
+                mode = "RGBA"
+            # end if
+            img = Image.frombytes(mode, [pix.width, pix.height], pix.samples)
+            fake_file = BytesIO()
+            img.save(fake_file, "png")
+
+            content_type = 'image/png'
+            message = fake_file.getvalue()
+        else:
+            with open('cropper.html') as f:
+                html = f.read()
+            # end if
+
+            content_type = 'text/html'
+            message = bytes(html, "utf8")
+        # end if
+
+        self.protocol_version = "HTTP/1.1"
+        self.send_response(200)
+        self.send_header("Content-Length", str(len(message)))
+        self.send_header("Content-Type", content_type)
+        self.end_headers()
+
+        self.wfile.write(message)
+        return
+    # end def
+# end class
+
+
+def run():
+    server = ('', 80)
+    httpd = HTTPServer(server, RequestHandler)
+    httpd.serve_forever()
+try:
+    import pyto_ui as ui
+    web_view = ui.WebView()
+    web_view.load_html(html)
+
+    from background import BackgroundTask # For running in background
+
+    # Run the server in background
+    with BackgroundTask() as b:
+        run()
+    # end if
+except (ImportError, ModuleNotFoundError):
+    run()
+
